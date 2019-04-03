be4523eb85a888af5810edf0eef17dfc401a9314
panyl <pan1394@126.com>
2019-04-03 13:56:27 +0800

    common commit

--- SqlParser/src/com/linkstec/sql/SqlOrder.java
+++ SqlParser/src/com/linkstec/sql/SqlOrder.java
@@ -7,16 +7,43 @@ public class SqlOrder extends SqlNode{
 
 	private List<SqlField> items;
 	
+	public List<SqlField> getItems() {
+		return items;
+	}
+	
+	
+	@Override
+	protected void convert() {
+		super.convert();
+//		if (list == null || list.size() == 0) {
+//			nodes.add(new SqlNode("なし"));
+//			return nodes;
+//		}
+//		
+//		int l = list.size();
+//		if (l == 1) {
+//			String tmp = (list.get(0));
+//			int idx = -1;
+//			if(tmp.contains("ORDER BY")) {
+//				idx = tmp.indexOf("ORDER BY");
+//				tmp = tmp.substring(idx + 8);
+//				nodes.add(new SqlNode(tmp.trim()));
+//			}else {
+//				nodes.add(new SqlNode(tmp.trim()));
+//			}
+//		} else {
+//			System.err.println("==================not only one order===============");
+//		}
+	}
+	
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("SELECT ");
-		for(int i=0; i<items.size(); i++) {
-			if(i == 1) {
-				sb.append(", ");
-			}
-			sb.append(items.get(i));
+		if(items.isEmpty()) {
+			return "";
 		}
+		items.forEach(i -> sb.append(i + ","));
+		sb.setLength(sb.length() - 1);
 		return sb.toString();
 	}
 }
--- SqlParser/src/com/linkstec/sql/constants/SQLConstants.java
+++ SqlParser/src/com/linkstec/sql/constants/SQLConstants.java
@@ -6,6 +6,7 @@ public class SQLConstants {
 	public static String REGEX_SPLIT_CHAR_AS = " AS ";
 	public static String REGEX_SELECT = "SELECT";
 	public static String REGEX_FROM = "FROM";
+	public static String REGEX_ORDER_BY = "ORDERY BY";
 	public static String REGEX_SPLIT_CHAR_COMMA = "[,、]";
 	public static final String REGEX_SPLIT_CHAR_IN = " IN";
 	
@@ -13,6 +14,8 @@ public class SQLConstants {
 	public static final String CONDITON_JOIN = "ーー";
 	public static final String CONDITON_AND_OR[] = {" AND", " OR"};
 	public static final String CONDITON_EXISTS = "EXISTS";
+
+	public static final String[] ORDER_VALE = {" ASC", " DESC"};
 	
 	public static final String[] REGEX_START_PARENT = {"(","（"};
 	public static final String[] REGEX_END_PARENT = {")", "）"};
